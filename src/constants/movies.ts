aa47755b1c2878133de53f3d5d2b1bad7e550f6c
Bosnyak Bence <bosnyak.bence@gmail.com>
2024-02-18 14:18:26 +0100

    Created constants folder

---
+++ src/constants/movies.ts
@@ -0,0 +1,10 @@
+const movies: Record<string, string> = {
+  "The Phantom Menace": "https://swapi.dev/api/films/4/",
+  "Attack of the Clones": "https://swapi.dev/api/films/5/",
+  "Revenge of the Sith": "https://swapi.dev/api/films/6/",
+  "A New Hope": "https://swapi.dev/api/films/1/",
+  "The Empire Strikes Back": "https://swapi.dev/api/films/2/",
+  "Return of the Jedi": "https://swapi.dev/api/films/3/",
+};
+
+export default movies;
